58a2e2b183449f409ed4f1a1017949f33625a5ed
eliasgron1 <eliasgronholm03@gmail.com>
2023-09-26 19:39:08 +0300

    worked on a3 more

--- assignment3/assignment3_bonus1_2.cpp
+++ assignment3/assignment3_bonus1_2.cpp
@@ -3,6 +3,11 @@
 #include <time.h>
 using namespace std;
 
+
+
+void matrixSum(int matrix1[][],int matrix2[][], int sumOfMatrix[][], int sizesx[0] int sizesy[0]);
+
+
 int main(){ 
 srand(time(NULL));
 
@@ -11,7 +16,7 @@ int sizesy[2];
 
 for(int i=0; i<2; i++){
 sizesx[i] = rand() % 3 + 3;
-sizesy[i] = rand() % 2 + 1;
+sizesy[i] = sizesx[i];
 cout << sizesx[i] << " ";
 cout << sizesy[i] << "   ";
 }
@@ -19,7 +24,7 @@ cout << endl;
 
 int matrix1[sizesy[0]][sizesx[0]];
 int matrix2[sizesy[1]][sizesx[1]];
-
+int sumOfMatrix[sizesx[0]][sizesy[0]];
 
 
 for(int i=0; i<=sizesy[0]; i++){             //values for matrix1
@@ -50,7 +55,19 @@ for(int i=0; i<=sizesy[1]; i++){             //print values for matrix2
     }
     cout << endl;
 }
+matrixSum(matrix1[][], matrix2[][], sumOfMatrix[][], sizesx[0], sizesy[0])
 
 return 0;
 }
 
+
+void matrixSum(int matrix1[][],int matrix2[][], int sumOfMatrix[][], int sizesx[0] int sizesy[0]){
+cout << endl;
+for(int i=0; i<sizesx[0]; i++){
+    for(int j=0; j<sizesy[0]; j++){
+        sumOfMatrix[j][i]=(matrix1[j][i] + matrix2[j][i]);
+        cout << sumOfMatrix[j][i] << " ";
+    }
+    cout << endl;
+}
+}
