37b14f51301b5e09ce101024f432565a21ee1240
Elias <e2203097@edu.vamk.fi>
2023-09-27 21:17:58 +0300

    more shit on a3

--- assignment3/assignment3_bonus1.cpp
+++ assignment3/assignment3_bonus1.cpp
@@ -1,48 +1,71 @@
-#include <cstdlib>
 #include <iostream>
-#include <time.h>
+#include <ctime>
+#include <iomanip>
+#include <cstdlib>
+
 using namespace std;
 
-int main(){ //main
+const int ROW_SIZE = 10;
+const int COLUMN_SIZE = 10;
 
-int sizes[4];
+void initialize(int [][10], int, int);
+void display(int matrix[][10], int, int);
+void sum(int matrix[][COLUMN_SIZE], int, int);
 
-srand(time(NULL));
-for(int i=0;i<4;i++){
-sizes[i] = rand() % 3 + 5;
-cout << sizes[i] << " ";
+int main() {
+
+    int matrix [ROW_SIZE][COLUMN_SIZE];
+
+    initialize(matrix, ROW_SIZE, COLUMN_SIZE);
+
+    display(matrix, ROW_SIZE,COLUMN_SIZE);
+
+    sum(matrix, ROW_SIZE,COLUMN_SIZE);
+
+
+    return 0;
 }
-cout << endl << endl;
-
-int matrix1[sizes[1]][sizes[2]];
-int matrix2[sizes[3]][sizes[3]];
 
 
-for(int i=0; i<sizes[1];i++){
-    for(int j=0; j<sizes[2];j++){
-        matrix1[j][i] = rand() % 5;
+
+void initialize(int matrix[][COLUMN_SIZE], int ROW_SIZE, int COLUMN_SIZE){
+    for (int i = 0; i < ROW_SIZE; i++){
+        for(int j = 0; j < COLUMN_SIZE; j++){
+            matrix[i][j] =  1 + rand() % 99;
+        }
     }
 }
 
 
-
-for(int i=0; i<sizes[1];i++){
-    for(int j=0; j<sizes[2];j++){
-        matrix2[j][i] = rand() % 5;
+void display(int matrix[][COLUMN_SIZE], int ROW_SIZE, int COLUMN_SIZE){
+    for(int i = 0; i < ROW_SIZE; i++){
+        for(int j = 0; j < COLUMN_SIZE; j++){
+            cout<< setw(4)<<matrix[i][j]<< " ";
+        }
+         cout<< endl;
     }
 }
 
 
+void sum(int matrix[][COLUMN_SIZE], int ROW_SIZE, int COLUMN_SIZE){
+    int sum_row;
+    int sum_col;
 
-for(int i=0; i<sizes[1];i++){
-    for(int j=0; j<sizes[2];j++){
-        cout << matrix1[j][i] << " ";
+    for(int i = 0; i < ROW_SIZE; i++){
+        int sum_row = 0;
+        for(int j = 0; j < COLUMN_SIZE; j++){
+            sum_row = sum_row + matrix[i][j];
+        }
     }
-    cout << endl;
-}
 
-}
+    for(int i = 0; i < ROW_SIZE; i++){
+        int sum_col = 0;
+        for(int j = 0; j < COLUMN_SIZE; j++){
+            sum_col = sum_col + matrix[i][j];
+        }
+    }
 
-void matrixSum(){
+    int sum = sum_row + sum_col;
 
+    cout<<"The sum of the matrix is "<<sum<< endl;
 }
--- assignment3/assignment3_bonus1_2.cpp
+++ assignment3/assignment3_bonus1_2.cpp
@@ -4,74 +4,86 @@
 using namespace std;
 
 
-int getSizes(int *pSizesx, int *pSizesy);
+const int MAX_COL1=5, MAX_ROW1=5;
+const int MAX_COL2=5, MAX_ROW2=5;
+
+void getSizes(int *pSizesx, int *pSizesy);
+void getMatrix1Values(int matrix1[][MAX_COL1], int, int, int*, int* );
+void getMatrix2Values(int matrix2[][MAX_COL2], int, int, int*, int* );
 
 
 int main(){ 
 srand(time(NULL));
 
 
+int matrix1[MAX_ROW1][MAX_COL1];
+int matrix2[MAX_ROW2][MAX_COL2];
+
 int sizesx[2];
 int sizesy[2];
 
 int *pSizesx = sizesx;
 int *pSizesy = sizesy;
+
 getSizes(pSizesx, pSizesy);
+getMatrix1Values(matrix1, MAX_ROW1, MAX_COL1, pSizesx, pSizesy);
+getMatrix2Values(matrix2, MAX_ROW2, MAX_COL2, pSizesx, pSizesy);
 
 
 
-int matrix1[*(pSizesy+0)][*(pSizesx+0)];
-int matrix2[*(pSizesy+1)][*(pSizesx+1)];
-
-
-for(int i=0; i < *(pSizesy+0); i++){             //values for matrix1
-    for(int j=0; j < *(pSizesx+0); j++){
-        matrix1[i][j]=rand() % 5;
-    }
-}
-
-for(int i=0; i < *(pSizesy+1); i++){             //values for matrix2
-    for(int j=0; j < *(pSizesx+1); j++){
-        matrix2[i][j]=rand() % 5;
-    }
-}
-
-
-
-cout << endl << endl;
-for(int i=0; i<sizesy[0]; i++){             //print values for matrix1
-    for(int j=0; j<sizesx[0]; j++){
+for(int i=0; i<MAX_ROW1-*(pSizesy+0); i++){             //print values for matrix1
+    for(int j=0; j<MAX_ROW1-*(pSizesx+0); j++){
         cout << matrix1[i][j] << " ";
     }
     cout << endl;
 }
-
 cout << endl << endl;
-for(int i=0; i<sizesy[1]; i++){             //print values for matrix2
-    for(int j=0; j<sizesx[1]; j++){
+for(int i=0; i<MAX_ROW2-*(pSizesy+1); i++){             //print values for matrix2
+    for(int j=0; j<MAX_ROW2-*(pSizesx+1); j++){
         cout << matrix2[i][j] << " ";
     }
     cout << endl;
 }
 
+cout << endl << "ROWS " << MAX_ROW1-*(pSizesy+0);
 
 return 0;
 }
 
 
-
-
-int getSizes(int *pSizesx, int *pSizesy){
+void getSizes(int *pSizesx, int *pSizesy){
 for(int i=0; i<2; i++){
-    *(pSizesx + i) = 3;                                // add rand() here
+    *(pSizesx + i) = 1;                                // add rand() here
     *(pSizesy + i) = *(pSizesx + i);
     cout << *(pSizesx + i)  << " ";
     cout << *(pSizesy + i)  << "   ";
 }
-cout << endl;
+cout << endl << endl;
 
-return 0;
 }
+
+
+
+void getMatrix1Values(int matrix1[][MAX_COL1], int MAX_ROW1, int MAX_COL1, int* pSizesy, int* pSizesx ){
+for(int i=0; i < MAX_ROW1-*(pSizesy+0); i++){             //values for matrix1
+    for(int j=0; j < MAX_COL1-*(pSizesx+0); j++){
+        matrix1[i][j]=rand() % 5;
+    }
+}
+
+}
+
+
+
+void getMatrix2Values(int matrix2[][MAX_COL2], int MAX_ROW2, int MAX_COL2, int* pSizesy, int* pSizesx){
+for(int i=0; i < MAX_ROW2-*(pSizesy+1); i++){             //values for matrix2
+    for(int j=0; j < MAX_COL2-*(pSizesx+1); j++){
+        matrix2[i][j]=rand() % 5;
+    }
+}
+
+}
+
 
 
 
